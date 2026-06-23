823f0d059d5ac56b6768168990e93cfb54a8feaa
Mihir Kanti Roy <mihirkantiroy2002@gmail.com>
2026-06-23 23:35:07 +0530

    UPDATE: Updated the server.js file as well as the frontend files

--- Backend/server.js
+++ Backend/server.js
@@ -12,6 +12,26 @@ app.get("/secretDetails", function(request, response){
     response.sendFile("C:/Users/mihir/OneDrive/Desktop/Classes/Duke Backend Classes/Secret Keeper/Frontend/secretDetails.html")
 })
 
+app.post("/signUp", function(request, response){
+    var username = request.body.user
+    var password = request.body.pass
+    //store in a DB
+})
+
+app.post("/signIn", function(request, response){
+    var username = request.body.user
+    var password = request.body.pass
+    //we need to check if this username and password is already there in the Database.
+})
+
+app.post("/submit", function(request, response){
+    var username = request.body.username
+    var age = request.body.age
+    var secret = request.body.secret
+    //The first step is finding the username in our database
+    //The second step is storing the age and the secret wrt the username
+})
+
 app.listen(80, function(){
     console.log("The server has started")
 })
